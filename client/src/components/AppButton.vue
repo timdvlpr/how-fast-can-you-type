08a7dc5911cc955c455d64e8326ecae9e9cfd536
timdvlpr <81222680+timdvlpr@users.noreply.github.com>
2022-06-08 14:27:44 +0200

    app button component added

---
+++ client/src/components/AppButton.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+interface Props {
+  icon?: string;
+  text: string;
+  selected?: boolean;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <button :class="{ selected: props.selected }">
+    <font-awesome-icon v-if="props.icon" :icon="props.icon"></font-awesome-icon>
+    <span>{{ props.text }}</span>
+  </button>
+</template>
+
+<style scoped lang="scss">
+button {
+  background: $main;
+  padding: 0.5rem 1rem;
+  border-radius: 1rem;
+  color: $light;
+  border: 2px solid $shade;
+  margin: 0 0.5rem;
+  cursor: pointer;
+  transition: 0.25s;
+  @include flex-center;
+  svg {
+    margin-right: 0.25rem;
+  }
+  &:hover {
+    background: $accent;
+  }
+}
+.selected {
+  background: $accent !important;
+}
+</style>
