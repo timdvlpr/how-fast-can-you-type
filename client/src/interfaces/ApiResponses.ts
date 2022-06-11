bc1c87bdb7478c2e686b5e72a8a076542e9c4200
timdvlpr <81222680+timdvlpr@users.noreply.github.com>
2022-06-11 11:17:34 +0200

    service functionality added for getting highscores of a specific language

--- client/src/interfaces/ApiResponses.ts
+++ client/src/interfaces/ApiResponses.ts
@@ -1,4 +1,11 @@
+import type { Highscore } from "@/interfaces/Highscore";
+
 export interface CheckHighscoreResponse {
   isHighscore: boolean;
   language: string;
+}
+
+export interface GetHighscoresResponse {
+  count: number;
+  data: Highscore[];
 }
--- client/src/services/highscoreService.ts
+++ client/src/services/highscoreService.ts
@@ -1,7 +1,20 @@
 import axios from "axios";
 import { CONFIG } from "@/config";
-import type { CheckHighscoreResponse } from "@/interfaces/ApiResponses";
+import type {
+  CheckHighscoreResponse,
+  GetHighscoresResponse,
+} from "@/interfaces/ApiResponses";
 import type { Highscore } from "@/interfaces/Highscore";
+
+export function getHighscoresOfLanguage(
+  language: string,
+  take: number,
+  skip: number
+): Promise<GetHighscoresResponse> {
+  return axios
+    .get(`${CONFIG.API_URL}/highscore/${language}/?take=${take}&skip=${skip}`)
+    .then((response) => response.data);
+}
 
 export function checkIsHighscore(
   language: string,
